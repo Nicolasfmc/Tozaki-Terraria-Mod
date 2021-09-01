8142a5c08c77d2dffe7034f872369fb45cfc9205
Nicolasfmc <56737194+Nicolasfmc@users.noreply.github.com>
2021-08-31 22:00:54 -0300

    V1.0

---
+++ Tozaki/Items/EspadaDeTerra.cs
@@ -0,0 +1,37 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Tozaki.Items
+{
+	public class EspadaDeTerra : ModItem
+	{
+		public override void SetStaticDefaults() 
+		{
+			Tooltip.SetDefault("Espada de terra.");
+		}
+
+		public override void SetDefaults() 
+		{
+			item.damage = 10;
+			item.melee = true;
+			item.width = 40;
+			item.height = 40;
+			item.useTime = 20;
+			item.useAnimation = 20;
+			item.useStyle = 1;
+			item.knockBack = 6;
+			item.value = 10000;
+			item.rare = 1;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = true;
+		}
+
+		public override void AddRecipes() 
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.DirtBlock, 10);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
---
+++ Tozaki/Tozaki.cs
@@ -0,0 +1,8 @@
+using Terraria.ModLoader;
+
+namespace Tozaki
+{
+	public class Tozaki : Mod
+	{
+	}
+}
